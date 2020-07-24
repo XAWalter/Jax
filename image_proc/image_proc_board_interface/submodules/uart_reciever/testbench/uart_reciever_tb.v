e8d07d7cb6602605f9a3656f9acb23533ff91934
XAWalter <xwalt001@ucr.edu>
2020-07-23 23:07:22 -0700

    Created image proc hierarchy

---
+++ image_proc/image_proc_board_interface/submodules/uart_reciever/testbench/uart_reciever_tb.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+module uart_reciever_tb;
+
+
+
+
+
+
+	initial begin
+		$dumpfile(`DUMP_FILE_NAME);
+		$dumpvars(0, uart_reciever_tb);
+	end
+endmodule
+
---
+++ image_proc/image_proc_board_interface/testbench/image_proc_board_interface_tb.v
@@ -0,0 +1,14 @@
+`timescale 1ns/1ps
+module image_proc_board_interface_tb;
+
+
+
+
+
+
+	initial begin
+		$dumpfile(`DUMP_FILE_NAME);
+		$dumpvars(0, image_proc_board_interface_tb);
+	end
+endmodule
+
